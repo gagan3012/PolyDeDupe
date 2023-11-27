193c8d083022b89e153d6b210fd265b055b10599
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2023-11-27 01:34:04 -0800

    Update prepare_changelog.py

--- scripts/prepare_changelog.py
+++ scripts/prepare_changelog.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from pathlib import Path
 
-from my_package.version import VERSION
+from PolyDeDupe.version import VERSION
 
 
 def main():
@@ -27,7 +27,7 @@ def main():
     lines.insert(insert_index, "\n")
     lines.insert(
         insert_index + 1,
-        f"## [v{VERSION}](https://github.com/allenai/python-package-template/releases/tag/v{VERSION}) - "
+        f"## [v{VERSION}](https://github.com/gagan3012/PolyDeDupe/releases/tag/v{VERSION}) - "
         f"{datetime.now().strftime('%Y-%m-%d')}\n",
     )
 
