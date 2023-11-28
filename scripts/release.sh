681ed0260c4214260024b4fc6564a9e45f41cec0
Gagan Bhatia <gbhatia880@gmail.com>
2023-11-27 23:00:32 -0800

    Bump version to v0.10.0 for release

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,18 +2,22 @@
 
 set -e
 
+echo "Let's create a new release!"
+
+python scripts/upgrade_version.py
+
 TAG=$(python -c 'from PolyDeDupe.version import VERSION; print("v" + VERSION)')
 
 read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
 
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
     python scripts/prepare_changelog.py
-    python scripts/release_notes.py
     git add -A
     git commit -m "Bump version to $TAG for release" || true && git push
     echo "Creating new git tag $TAG"
     git tag "$TAG" -m "$TAG"
     git push --tags
+    python scripts/release_notes.py
 else
     echo "Cancelled"
     exit 1
