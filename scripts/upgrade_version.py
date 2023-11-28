3334db15c50d1976923fc34c4b8b174e3ab05061
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2023-11-28 01:41:03 -0800

    Formats

--- scripts/upgrade_version.py
+++ scripts/upgrade_version.py
@@ -1,18 +1,18 @@
 # Filename containing the version information
-filename = 'PolyDeDupe/version.py'
+filename = "PolyDeDupe/version.py"
 
 # Read the current version information
-with open(filename, 'r+', encoding='utf-8') as file:
+with open(filename, "r+", encoding="utf-8") as file:
     lines = file.readlines()
 
 
 for i, line in enumerate(lines):
-    if '_MINOR' in line:
-        parts = line.split('=')
+    if "_MINOR" in line:
+        parts = line.split("=")
         minor_version = int(parts[1].strip().strip('"')) + 1
         lines[i] = '_MINOR = "{}"\n'.format(minor_version)
         break
 
 # Write the updated version information back to the file
-with open(filename, 'r+', encoding='utf-8') as file:
+with open(filename, "r+", encoding="utf-8") as file:
     file.writelines(lines)
